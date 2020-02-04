b3f37cc995dcba74b3cfaa1b8b39b61da0b7d734
cake <jarrettw@protonmail.com>
2020-02-04 16:10:31 -0500

    refactored with define

--- ch1/fahrenheit-celsius-for-2.c
+++ ch1/fahrenheit-celsius-for-2.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit of table */
+#define STEP 20 /* step size */
+
 int main(void)
 {
 /* print Fahrenheit-Celsius table */
     int fahrenheit;
     /* prints Fahrenheit-Celsius in reverse i.e, 300 - 0 */
-    for (fahrenheit = 300; fahrenheit >= 0; fahrenheit = fahrenheit - 20)
+    for (fahrenheit = LOWER; fahrenheit >= UPPER; fahrenheit = fahrenheit - STEP)
         printf("%3d %6.1f\n", fahrenheit, (5.0/9.0) * (fahrenheit-32));
 }
--- ch1/fahrenheit-celsius-for.c
+++ ch1/fahrenheit-celsius-for.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit of table */
+#define STEP 20 /* step size */
+
 int main(void)
 {
 /* print Fahrenheit-Celsius table */
     int fahrenheit;
-    for (fahrenheit = 0; fahrenheit <= 300; fahrenheit = fahrenheit + 20)
+    for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP)
         printf("%3d %6.1f\n", fahrenheit, (5.0/9.0) * (fahrenheit-32));
 }
